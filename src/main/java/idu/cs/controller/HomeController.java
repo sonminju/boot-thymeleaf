254454bea7112e760945cc973460898f3b72e04e
minju <alswn1589@email.com>
2019-05-15 17:13:35 +0900

    commit20190515

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import idu.cs.domain.User;
+import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
@@ -21,15 +23,28 @@ public class HomeController {
 		model.addAttribute("egy", "유응구");
 		return "index";
 	}
-	@GetMapping("/users")
+	@GetMapping("/register") //등록폼, form.html로 감
+	public String getRegForm(Model model) {
+		return "form";
+	}
+	@GetMapping("/users") //GET방식
 	public String getAllUser(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userlist";
 	}
-	@PostMapping("/users")
+	@PostMapping("/users") //POST방식
 	public String createUser(@Valid @RequestBody User user, Model model) {
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
+	}
+	@GetMapping("/users/{id}")
+	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		model.addAttribute("name", user.getName());
+		model.addAttribute("company", user.getCompany());
+		return "user";
+		//return ResponseEntity.ok().body(user);
 	}
 }
