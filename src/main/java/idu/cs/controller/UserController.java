67c7aa0efcff4f6b27a60c85b84b4623eae23771
minju <alswn1589@gmail.com>
2019-06-05 18:04:18 +0900

    0605 수업내용

--- src/main/java/idu/cs/controller/UserController.java
+++ src/main/java/idu/cs/controller/UserController.java
@@ -15,28 +15,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import idu.cs.domain.User;
+import com.mysql.cj.Session;
+
+import idu.cs.domin.User;
+import idu.cs.entity.UserEntity;
 import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
+import idu.cs.service.UserService;
 
 @Controller // @Component, @Service, @Repository
 // Spring Framework에게 이 클래스로부터 작성된 객체는 Controller 역할을 함을 알려줌
 // Spring 이 이 클래스로부터 Bean객체를 생성해서 등록할 수 있음
 public class UserController {
-	@Autowired UserRepository userRepo; // Dependency Injection
+	@Autowired UserService userService;
 	
 	@GetMapping("/")
 	public String home(Model model) { 
 		return "index";
 	}
+	
 	@GetMapping("/user-login-form")
 	public String getLoginForm(Model model) {
 		return "login";
 	}
 	@PostMapping("/login")
-	public String loginUser(@Valid User user, HttpSession session) {
+	public String loginUser(@Valid UserEntity user, HttpSession session) {
 		System.out.println("login process : ");
-		User sessionUser = userRepo.findByUserId(user.getUserId());
+		User sessionUser = userService.getUserByUserId(user.getUserId());
 		if(sessionUser == null) {
 			System.out.println("id error : ");
 			return "redirect:/user-login-form";
@@ -48,34 +53,48 @@ public class UserController {
 		session.setAttribute("user", sessionUser);
 		return "redirect:/";
 	}
+	
 	@GetMapping("/logout")
 	public String logoutUser(HttpSession session) {
 		session.removeAttribute("user"); //객체에서 user가 없어지는 것
 		// session.invalidate();
 		return "redirect:/";
 	}
+	
+	@GetMapping("/users")
+	public String getAllUser(Model model) {
+		model.addAttribute("users", userService.getUsers());
+		return "userlist";
+	}
+	
 	@GetMapping("/user-register-form")
 	public String getRegForm(Model model) {
 		return "register";
 	}
-	@GetMapping("/users")
-	public String getAllUser(Model model) {
-		model.addAttribute("users", userRepo.findAll());
-		return "userlist";
-	}
+	
 	@PostMapping("/users")
 	public String createUser(@Valid User user, Model model) {		
-		if(userRepo.save(user) != null)
-			System.out.println("Database 등록 성공");
-		else
-			System.out.println("Database 등록 실패");
-		model.addAttribute("users", userRepo.findAll());
-		return "redirect:/users";
+		userService.saveUser(user);
+		return "redirect:/users"; // get 방식으로 해당 url로 재지정함
 	}
+	
+	@GetMapping("/user-update-form")
+	public String getUpdateForm(Model model, HttpSession session) {
+		// 서비스를 통해 리파지터리로 부터 정보를 가져와야 하나 세션에 저장해두었으므로 정보를 활용
+		User user = (User) session.getAttribute("user"); //정보를 가져옴
+		
+		/*
+		User sessionUser = userService.getUserById(user.getId());
+		model.addAttribute("user", sessionUser);
+		*/
+		model.addAttribute("user", user);
+		return "info";
+	}
+	/*
 	@GetMapping("/users/{id}")
 	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
 			throws ResourceNotFoundException {
-		User user = userRepo.findById(userId).get();//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		UserEntity user = userRepo.findById(userId).get();//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
 		model.addAttribute("user", user);
 		return "user";
 		//return ResponseEntity.ok().body(user);
@@ -83,26 +102,30 @@ public class UserController {
 	@GetMapping("/users/fn")
 	public String getUserByName(@Param(value="name") String name, Model model)
 			throws ResourceNotFoundException {
-		List<User> users = userRepo.findByName(name);//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		List<UserEntity> users = userRepo.findByName(name);//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
 		model.addAttribute("users", users);
 		return "userlist";
 		//return ResponseEntity.ok().body(user);
 	}
+	*/
 	@PutMapping("/users/{id}") // @PatchMapping
-	public String updateUser(@PathVariable(value = "id") Long userId, 
-			@Valid User userDetails, Model model) {		
-		User user = userRepo.findById(userId).get(); 
-		// user는 DB로 부터 읽어온 객체
-		user.setName(userDetails.getName()); // userDetails는 전송한 객체
-		user.setCompany(userDetails.getCompany());
-		userRepo.save(user);
+	public String updateUser(@PathVariable(value = "id") Long id, 
+			@Valid User user, Model model, HttpSession session) {	
+		/*
+		 * updateUser 객체는 입력 폼 내용 : id 값이 없음, 
+		 */
+		user.setId(userService.getUserById(id).getId());
+		userService.updateUser(user);
+		session.setAttribute("user", user);
 		return "redirect:/users";
 	}
+	/*
 	@DeleteMapping("/users/{id}")
 	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {		
-		User user = userRepo.findById(userId).get();
+		UserEntity user = userRepo.findById(userId).get();
 		userRepo.delete(user);
 		model.addAttribute("name", user.getName());
 		return "user-deleted";
-	}
+	}*/
+	
 }
--- src/main/java/idu/cs/entity/UserEntity.java
+++ src/main/java/idu/cs/entity/UserEntity.java
@@ -1,4 +1,4 @@
-package idu.cs.domain;
+package idu.cs.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,34 +7,31 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import idu.cs.domin.User;
+
 @Entity
-@Table(name = "user_table")
-public class User {
+@Table(name = "user")
+public class UserEntity {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id; 
-	// database에서 sequence number, auto increment => primary key 역할
-	
-	@Column(nullable=false, length=20, unique=true)
+//	@OrderBy
+	private Long id; // database에서 sequence number, primary key 역할
+	@Column(nullable=false, length=20, unique=true) // null 하용 안함, 유일키
 	private String userId;
+	@Column(nullable=false)
 	private String userPw;
 	private String name;
 	private String company;
 	
-	public User() {}
-	
-	public User(String userId, String userPw, String name, String company) {
-		super();
-		this.userId = userId;
-		this.userPw = userPw;
-		this.name = name;
-		this.company = company;
-	} //생성자만들기
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getUserId() {
 		return userId;
 	}
-	
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
@@ -43,12 +40,6 @@ public class User {
 	}
 	public void setUserPw(String userPw) {
 		this.userPw = userPw;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
 	}
 	public String getName() {
 		return name;
@@ -61,8 +52,25 @@ public class User {
 	}
 	public void setCompany(String company) {
 		this.company = company;
+	}	
+	
+	public String toString() {
+		return "id : " + userId + ", name : " + name + ", company : " + company;
 	}
 	
+	public User buildDomain() {
+		User user = new User();
+		user.setUserId(userId);
+		user.setUserPw(userPw);
+		user.setName(name);
+		user.setCompany(company);
+		return user;
+	}
 	
-	
+	public void buildEntity(User user) {
+		userId = user.getUserId();
+		userPw = user.getUserPw();
+		name = user.getName();
+		company = user.getCompany();
+	}
 }
